04421eb9e9b458a111b1a1209d3a80899eb558dc
Riley <60585354+rsm5209@users.noreply.github.com>
2025-02-27 16:12:29 -0500

    Added EoS files

---
+++ AV14+UVII EoS.py
@@ -0,0 +1,9 @@
+# modules
+import matplotlib.pyplot as py
+import numpy as np
+
+# functions
+
+# constants in cgs units
+plancks_constant = 6.6261E-27 
+reduced_plancks_constant = plancks_constant / 2
